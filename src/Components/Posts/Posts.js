68cc3b2eda3c946545c0a5dae399953570c9ab54
kiraa123 <kiranprakash970@gmail.com>
2024-03-21 13:26:52 +0530

    post products

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React,{useState,useEffect,useContext} from 'react';
+import {firestore} from '../../firebase/config'
+import { collection,getDocs } from '@firebase/firestore';
 
 import Heart from '../../assets/Heart';
 import './Posts.css';
+import { FirebaseContext } from '../../store/FirebaseContext';
+import { PostContext } from '../../store/ViewContext';
+import { useNavigate } from 'react-router-dom'; 
 
 function Posts() {
+    const navigate= useNavigate();
+    const {firebase}= useContext(FirebaseContext)
+    const [products,setProducts]=useState([])
+    const [loading,setLoading]=useState(false);
+    const {setPostDetails}=useContext(PostContext)
+    useEffect(()=>{
+        const fetchProducts = async ()=>{
+            setLoading(true);
+            try{
+                const querySnapshot= await getDocs(collection(firestore,'products'));
+                const productsData= [];
+                querySnapshot.forEach((doc)=>{
+                    productsData.push({id:doc.id,...doc.data()});
+                });
+                setProducts(productsData);
+            }catch (error){
+                console.error('Error fetching products: ', error);
+            }
+            setLoading(false);
+        };
+        fetchProducts();
+    },[]);
 
   return (
     <div className="postParentDiv">
@@ -13,24 +40,31 @@ function Posts() {
           <span>View more</span>
         </div>
         <div className="cards">
-          <div
-            className="card"
-          >
+            {
+            products.map(product =>{
+                return <div className='card'
+                onClick={()=>{
+                    setPostDetails(product)
+                    navigate('/view')
+                }}>
+           
             <div className="favorite">
               <Heart></Heart>
             </div>
             <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
+              <img src={product.image} alt="" />
             </div>
             <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
+              <p className="rate">&#x20B9; {product.price}</p>
+              <span className="kilometer">{product.category}</span>
+              <p className="name"> {product.name}</p>
             </div>
             <div className="date">
-              <span>Tue May 04 2021</span>
+              <span>{product.createAt}</span>
             </div>
           </div>
+            })
+        }
         </div>
       </div>
       <div className="recommendations">
